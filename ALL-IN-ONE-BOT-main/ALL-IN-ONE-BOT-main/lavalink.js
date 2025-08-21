72d7889f0bd3e8543f41d96efbdf65b5085c321b
dhruvsh1997 <dhruvsh1997@gmail.com>
2025-08-21 11:24:44 +0530

    Attribure js added

---
+++ ALL-IN-ONE-BOT-main/ALL-IN-ONE-BOT-main/lavalink.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    enabled: true, 
+    lavalink: {
+      name: "GlaceYT",
+      password: "glace",
+      host: "5.39.63.207",
+      port:  8262,
+      secure: false
+    }
+};
+
